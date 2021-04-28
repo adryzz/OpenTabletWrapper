c01d969d498ae3d45004cc1848a8b764bbba350f
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-28 12:02:45 +0200

    Add project files.

---
+++ OpenTabletWrapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace OpenTabletWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process daemon = new Process();
+            daemon.StartInfo.FileName = "OpenTabletDriver.Daemon.exe";
+            //daemon.StartInfo.WorkingDirectory = Assembly.GetExecutingAssembly().Location;
+            daemon.StartInfo.UseShellExecute = false;
+            daemon.StartInfo.CreateNoWindow = true;
+            daemon.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            daemon.Start();
+        }
+    }
+}
